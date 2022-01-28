aa01cdf99a37c38c8393bbfcbd18df7d8261f7a8
syedhaq5511 <syedhaq.merndev@gmail.com>
2022-01-28 09:58:56 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import UIReduxCard  from './redux-ground/displayComp'
+import UIReduxCard  from './redux-ground/ui-redux.js'
 import { CoinProvider } from './context-ground/coindataContext'
 import UIContextCard from './context-ground/ui-context'
 
